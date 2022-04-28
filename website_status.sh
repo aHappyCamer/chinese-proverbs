afaf0f616f59593db48b413703b65ff6b408d8cd
aHappyCamer <joeydcam@gmail.com>
2022-04-28 15:17:48 -0400

    Revert to original file

--- website_status.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if curl -s --head  --request GET https://chineseproverbs.duckdns.org | grep "200 OK" > /dev/null; then     
-    echo "website is UP"; 
-else    
-    echo "website is DOWN"; 
-fi
